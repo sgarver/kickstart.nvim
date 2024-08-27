4a09f98b60e5d196e26e0c3ef5c72b772d8abf95
Stephen Garver <stepgar@gmail.com>
2024-08-27 06:08:43 -0700

    Add auto pairs plugin, change default tab to 4 spaces

--- init.lua
+++ init.lua
@@ -157,6 +157,7 @@ vim.opt.scrolloff = 10
 -- custom stuff
 vim.opt.relativenumber = true
 vim.opt.colorcolumn = '100'
+vim.opt.tabstop = 4
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
@@ -887,7 +888,7 @@ require('lazy').setup({
   -- require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.autopairs',
   -- require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
