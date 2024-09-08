d985445963bd036400be625a8a36aad988a8f344
Stephen Garver <stepgar@gmail.com>
2024-09-08 05:50:07 -0700

    Add vim fugitive

--- init.lua
+++ init.lua
@@ -239,6 +239,8 @@ require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
+  'tpope/vim-fugitive', -- run git commands from within vim
+
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
